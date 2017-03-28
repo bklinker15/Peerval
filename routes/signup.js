cf94d830a035d9c7ebc392d79dc3e39686c60624
Anish Verma <anishver@usc.edu>
2017-03-27 17:52:29 -0700

    copied signup.js from pages

---
+++ routes/signup.js
@@ -0,0 +1,17 @@
+/**
+ * Created by bwbas on 3/23/2017.
+ */
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    res.render('signup');
+});
+
+//signup user route
+router.post('/signupUser', function(req, res, next){
+    console.log("username is " + req.body.username);
+});
+
+module.exports = router;
