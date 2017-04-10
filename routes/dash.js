2d35e997a6ee6418b84e0b0dc5efa8f246998161
Anish Verma <anishver@usc.edu>
2017-04-10 01:50:01 -0700

    fixed dashboard connection

--- routes/dash.js
+++ routes/dash.js
@@ -16,34 +16,38 @@ router.get('/', ensureAuthenticated, function(req, res, next) {
       console.log(user.uploadedEssayIds);
         //loop through all the essay IDs for this user
        // for( var essayID = 0; essayID< user.uploadedEssayIds.length(); essayID++){
-        var ctr=0;
-        user.uploadedEssayIds.forEach(function(essayID) {
+        if(user.uploadedEssayIds.length==0){
+            res.render('dash', { title: 'Express' , essayArray: essays});
+        }else {
+            var ctr = 0;
+            user.uploadedEssayIds.forEach(function (essayID) {
 
-            //find the essay
-            Essay.findById(essayID, function (err, essay) {
-                //get the thumbnail
-                var google = require('googleapis');
-                var drive = google.drive({ version: 'v2', auth: global.myGoogleAuth });
+                //find the essay
+                Essay.findById(essayID, function (err, essay) {
+                    //get the thumbnail
+                    var google = require('googleapis');
+                    var drive = google.drive({version: 'v2', auth: global.myGoogleAuth});
 
-                drive.files.get({ fileId: essay.fileId }, function(err, result) {
-                    var thumbnail = result.thumbnailLink;
-                    //add essay object to the array
-                    essays.push({
-                        title: essay.title,
-                        thumbnailLink: thumbnail,
-                        status: essay.status,
-                        date: essay.uploadDate,
-                        subject: essay.subject
-                    })
-                    ctr++;
-                    if(ctr == user.uploadedEssayIds.length){
-                        console.log(essays[0]);
-                        res.render('dash', { title: 'Express' , essayArray: essays});
-                    }
+                    drive.files.get({fileId: essay.fileId}, function (err, result) {
+                        var thumbnail = result.thumbnailLink;
+                        //add essay object to the array
+                        essays.push({
+                            title: essay.title,
+                            thumbnailLink: thumbnail,
+                            status: essay.status,
+                            date: essay.uploadDate,
+                            subject: essay.subject
+                        })
+                        ctr++;
+                        if (ctr == user.uploadedEssayIds.length) {
+                            console.log(essays[0]);
+                            res.render('dash', {title: 'Express', essayArray: essays});
+                        }
+                    });
                 });
-            });
 
-        });
+            });
+        }
     });
 });
 
